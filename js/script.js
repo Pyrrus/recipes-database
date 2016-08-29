30c7b9e1479750c98810ce5b888f245c3b141b1f
David Bethune <dbethune1970@gmail.com>
2016-08-29 16:36:05 -0700

    Add files via upload
    html added some function carousel added and working search bar and add favorite button and jumbotron

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.getElementById('button').onclick = (function() {
+    document.getElementsByTagName('audio')[0].play();
+    document.getElementsByTagName('span')[0].innerHTML = 'Hello bitchception!';
+    return false;
+});
+document.getElementById('button').onclick = (function() {
+    document.getElementsByTagName('audio')[0].play();
+    document.getElementsByTagName('span')[0].innerHTML = 'Hello bitchception!';
+    return false;
+});
