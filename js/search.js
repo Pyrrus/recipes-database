0edc52931f350d2719786666d11dd4ae9b29a812
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-08-30 15:47:10 -0700

    Add the comments in search js

--- js/search.js
+++ js/search.js
@@ -1,7 +1,12 @@
+// make the object for the fetcher 
+// to find the data from the database
 var fetcher = function () {
   this.found = [];
 }
 
+// This is the search algorithms to search though the 
+// database by the ingredient by the user. Once found the it
+// will add it to the array within the fetcher object
 fetcher.prototype.search = function (database, keyword) {
   var holder = [];
   database.forEach(function (data) {
@@ -17,6 +22,7 @@ fetcher.prototype.search = function (database, keyword) {
   this.found = holder;
 }
 
+// this will find the doubles in the found array
 fetcher.prototype.removedouble = function () {
   for (var i = 0; i < this.found.length; i++) {
     for (var j = i + 1; j < this.found.length; j++) {
