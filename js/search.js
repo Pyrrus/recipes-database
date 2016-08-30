e2e9381e23bca76342cbe03ce9926073d3ed6609
Adam Gorbahn <adam@>
2016-08-30 10:43:46 -0700

    make a search objects for the data

--- js/search.js
+++ js/search.js
@@ -1,33 +1,38 @@
 var test = ["egg", "rice", "wine"];
 
-var found = [];
+var fetcher = function () {
+  this.found = [];
+}
 
-function search() {
+fetcher.prototype.search = function (database, keyword) {
+  var holder = [];
   database.forEach(function (data) {
     for (var i = 0; i < data["keyword"].length; i++) {
-      for (var j = 0; j < test.length; j++) {
+      for (var j = 0; j < keyword.length; j++) {
         var string = data["keyword"][i].ingredient;
-        if (string.match(test[j])) {
-          found.push(data);
+        if (string.match(keyword[j])) {
+          holder.push(data);
         }
       }
     }
   });
+  this.found = holder;
 }
 
-
-function removeDouble() {
-  for (var i = 0; i < found.length; i++) {
-    for (var j = i + 1; j < found.length; j++) {
-      if (found[i].name === found[j].name) {
-        found.splice(j, 1);
+fetcher.prototype.removedouble = function () {
+  for (var i = 0; i < this.found.length; i++) {
+    for (var j = i + 1; j < this.found.length; j++) {
+      if (this.found[i].name === this.found[j].name) {
+        this.found.splice(j, 1);
       }
     }
   }
 }
 
-search();
+var search = new fetcher;
 
-removeDouble();
+search.search(database, test);
 
-console.log(found)
+search.removedouble();
+
+console.log(search.found);
